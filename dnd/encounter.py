9f23542c23a7bf8fad979f39f877cdfd64687486
Patrick McGurk <pdmcgurk@gmail.com>
2021-12-29 11:22:13 -0600

    Clear compiler warnings for PEP format etc

--- dnd/encounter.py
+++ dnd/encounter.py
@@ -1,5 +1,6 @@
+import os
+
 from functools import cmp_to_key, partial
-from os import name, system
 from random import randint
 from re import compile as compile_regex
 
@@ -7,310 +8,327 @@ INITIATIVE_DIE_SIZE = 10
 
 # Default colors available among Roll20 token icons
 COLORS = [
-  "Red",
-  "Blue",
-  "Green",
-  "Purple",
-  "Pink",
-  "Yellow",
-  "Brown"
+    "Red",
+    "Blue",
+    "Green",
+    "Purple",
+    "Pink",
+    "Yellow",
+    "Brown"
 ]
 
-party = [
-  ("Braaggi", 3),
-  ("Circe", 5),
-  ("Pipes", 7),
-  ("Tom", 6),
-  ("Vorbith", 2)
+PARTY = [
+    ("Braaggi", 3),
+    ("Circe", 5),
+    ("Pipes", 7),
+    ("Tom", 6),
+    ("Vorbith", 2)
 ]
 
 HP_PATTERN = compile_regex(r'(?P<hp>\d+)(\/(?P<max_hp>\d+))?')
 
-def clear():
-  # for windows
-  if name == 'nt':
-    _ = system('cls')
 
-  # for mac and linux(here, os.name is 'posix')
-  else:
-    _ = system('clear')
+def clear():
+    # for windows
+    if os.name == 'nt':
+        _ = os.system('cls')
+
+    # for mac and linux(here, os.name is 'posix')
+    else:
+        _ = os.system('clear')
+
 
 def main():
-  encounter = Encounter(party)
-  EncounterPlayer(encounter).play()
-  return
+    encounter = Encounter(PARTY)
+    EncounterPlayer(encounter).play()
+    return
+
 
 class Encounter:
 
-  def __init__(self, party):
-    self.party = party
-    self.pcs = self.roll_pcs()
-    self.monsters = self.add_monsters()
-    self.combatants = self.pcs + self.monsters
+    def __init__(self, party):
+        self.party = party
+        self.pcs = self.roll_pcs()
+        self.monsters = self.add_monsters()
+        self.combatants = self.pcs + self.monsters
 
-  def roll_pcs(self):
-    return [Combatant(
-      pc[0],
-      self.roll_initiative(pc[1]),
-      self.get_hit_points(pc[0])
-    ) for pc in self.party]
+    def roll_pcs(self):
+        return [Combatant(
+            pc[0],
+            self.roll_initiative(pc[1]),
+            self.get_hit_points(pc[0])
+        ) for pc in self.party]
 
-  def add_monsters(self):
-    monsters = []
-    add_more = True
-    while add_more:
-      monster_name = input("Enter a monster name (leave empty to continue): ")
-      if monster_name == '':
-        add_more = False
-      else:
-        monsters = monsters + self.add_monster(monster_name)
+    def add_monsters(self):
+        monsters = []
+        add_more = True
+        while add_more:
+            monster_name = input("Enter a monster name (leave empty to continue): ")
+            if monster_name == '':
+                add_more = False
+            else:
+                monsters = monsters + self.add_monster(monster_name)
 
-    return monsters
+        return monsters
 
-  def add_monster(self, monster_name):
-    names = self.get_multiple_names(monster_name)
-    initiative_bonus = self.get_initiative_bonus(monster_name)
-    hit_points = self.get_hit_points(monster_name)
-    return [Combatant(
-      n,
-      self.roll_initiative(initiative_bonus),
-      hit_points
-    ) for n in names]
+    def add_monster(self, monster_name):
+        names = self.get_multiple_names(monster_name)
+        initiative_bonus = self.get_initiative_bonus(monster_name)
+        hit_points = self.get_hit_points(monster_name)
+        return [Combatant(
+            n,
+            self.roll_initiative(initiative_bonus),
+            hit_points
+        ) for n in names]
 
-  def roll_initiative(self, initiative_bonus):
-    return randint(1, INITIATIVE_DIE_SIZE) + initiative_bonus
+    @staticmethod
+    def roll_initiative(initiative_bonus):
+        return randint(1, INITIATIVE_DIE_SIZE) + initiative_bonus
 
-  def get_hit_points(self, combatant_name):
-    while True:
-      hp = input("(Optional) Enter hit points for %s (y or x/y): " % combatant_name)
-      if hp == '':
-        return (0, 0)
-      else:
-        m = HP_PATTERN.match(hp)
-        if m:
-          if m.group("max_hp"):
-            return (int(m.group("hp")), int(m.group("max_hp")))
-          return (int(m.group("hp")), int(m.group("hp")))
-      print("Enter a number for maximum HP, or a ratio like 17/38")
+    @staticmethod
+    def get_hit_points(combatant_name):
+        while True:
+            hp = input("(Optional) Enter hit points for %s (y or x/y): " % combatant_name)
+            if hp == '':
+                return 0, 0
+            else:
+                m = HP_PATTERN.match(hp)
+                if m:
+                    if m.group("max_hp"):
+                        return int(m.group("hp")), int(m.group("max_hp"))
+                    return int(m.group("hp")), int(m.group("hp"))
+            print("Enter a number for maximum HP, or a ratio like 17/38")
 
-  def get_multiple_names(self, monster_name):
-    monster_number = self.get_number(monster_name)
-    if monster_number == 1:
-      return [monster_name]
-    return ["%s (%s)" % (monster_name, COLORS[i]) for i in range(monster_number)]
+    def get_multiple_names(self, monster_name):
+        monster_number = self.get_number(monster_name)
+        if monster_number == 1:
+            return [monster_name]
+        return ["%s (%s)" % (monster_name, COLORS[i]) for i in range(monster_number)]
 
-  def get_number(self, monster_name):
-    monster_number = 0
-    while 1 > monster_number or monster_number > len(COLORS):
-      try:
-        monster_number = int(input("How many %s?: " % monster_name))
-        if monster_number > len(COLORS):
-          print("Come on, switch it up")
-      except:
-        print("Try again")
-    return monster_number
+    @staticmethod
+    def get_number(monster_name):
+        monster_number = 0
+        while 1 > monster_number or monster_number > len(COLORS):
+            try:
+                monster_number = int(input("How many %s?: " % monster_name))
+                if monster_number > len(COLORS):
+                    print("Come on, switch it up")
+            except Exception:
+                print("Try again")
+        return monster_number
 
-  def get_initiative_bonus(self, monster_name):
-    initiative_bonus = None
-    while initiative_bonus == None:
-      try:
-        initiative_bonus = int(input("Enter initiative bonus for %s: " % monster_name))
-      except:
-        print("Try again")
-    return initiative_bonus
+    @staticmethod
+    def get_initiative_bonus(monster_name):
+        initiative_bonus = None
+        while initiative_bonus is None:
+            try:
+                initiative_bonus = int(input("Enter initiative bonus for %s: " % monster_name))
+            except Exception:
+                print("Try again")
+        return initiative_bonus
 
 
 class EncounterPlayer:
 
-  def __init__(self, encounter):
-    self.encounter = encounter
-    self.keep_playing = True
-    # self.sort = self.sort_by_initiative
-    # or
-    self.sort = self.sort_by_side
+    def __init__(self, encounter):
+        self.encounter = encounter
+        self.keep_playing = True
 
-  def play(self):
-    # NOTE: self.sort_by_side displays PCs followed by monsters rather than initiative order.
-    # To play with traditional initiative rules, replace it with self.sort_by_initiative.
-    self.active = self.encounter.combatants.copy()
-    self.ordered = self.sort_by_initiative(self.active)
-    self.acted = []
-    while self.keep_playing:
-      while self.keep_playing and self.ordered != []:
-        self.keep_playing = self.turn_prompt()
-      self.acted = []
-      self.ordered = self.sort(self.active)
-    return
+        # NOTE: self.sort_by_side displays PCs followed by monsters rather than initiative order.
+        # To play with traditional initiative rules, replace it with self.sort_by_initiative.
+        # self.sort = self.sort_by_initiative
+        # or
+        self.sort = self.sort_by_side
 
-  def sort_by_side(self, combatants):
-    return combatants.copy()
+        self.active = self.encounter.combatants.copy()
+        self.ordered = self.sort_by_initiative(self.active)
+        self.acted = []
 
-  def sort_by_initiative(self, combatants):
-    return sorted(combatants, key=cmp_to_key(self.initiative_comparator))
+    def play(self):
 
-  def initiative_comparator(self, combatant1, combatant2):
-    if combatant1.initiative > combatant2.initiative:
-      return -1
-    return 1
+        while self.keep_playing:
+            while self.keep_playing and self.ordered != []:
+                self.keep_playing = self.turn_prompt()
+            self.acted = []
+            self.ordered = self.sort(self.active)
+        return
 
-  def turn_prompt(self):
-    base_cases = {
-      "q": lambda: self.false(),
-      "s": lambda: self.empty_ordered(),
-      "d": lambda: self.damage_prompt(),
-      "h": lambda: self.heal_prompt()
-    }
-    switcher = OptionSwitcher(len(self.ordered), self.take_turn, base_cases)
+    @staticmethod
+    def sort_by_side(combatants):
+        return combatants.copy()
 
-    clear()
-    self.print_main_menu()
-    callback = None
-    while callback == None:
-      option = input(
-         "Select a number to take turn, or " +
-         "apply (d)amage or (h)ealing, (s)kip to next turn, or (q)uit: "
-      )
-      callback = switcher.get(option)
+    def sort_by_initiative(self, combatants):
+        return sorted(combatants, key=cmp_to_key(self.initiative_comparator))
 
-    return callback()
+    @staticmethod
+    def initiative_comparator(combatant1, combatant2):
+        if combatant1.initiative > combatant2.initiative:
+            return -1
+        return 1
 
-  def true(self):
-    return True
+    def turn_prompt(self):
+        base_cases = {
+            "q": lambda: self.false(),
+            "s": lambda: self.empty_ordered(),
+            "d": lambda: self.damage_prompt(),
+            "h": lambda: self.heal_prompt()
+        }
+        switcher = OptionSwitcher(len(self.ordered), self.take_turn, base_cases)
 
-  def false(self):
-    return False
+        clear()
+        self.print_main_menu()
+        callback = None
+        while callback is None:
+            option = input(
+                "Select a number to take turn, or " +
+                "apply (d)amage or (h)ealing, (s)kip to next turn, or (q)uit: "
+            )
+            callback = switcher.get(option)
 
-  def empty_ordered(self):
-    self.ordered = []
-    return True
+        return callback()
 
-  def damage_prompt(self):
-    base_cases = {
-      "b": lambda: self.true()
-    }
-    switcher = OptionSwitcher(len(self.active), self.apply_damage, base_cases)
+    @staticmethod
+    def true():
+        return True
 
-    clear()
-    self.print_damage_menu()
-    callback = None
-    while callback == None:
-      option = input("Select a number to damage combatant, or go (b)ack: ")
-      callback = switcher.get(option)
+    @staticmethod
+    def false():
+        return False
 
-    return callback()
+    def empty_ordered(self):
+        self.ordered = []
+        return True
 
-  def heal_prompt(self):
-    base_cases = {
-      "b": lambda: self.true()
-    }
-    switcher = OptionSwitcher(len(self.encounter.combatants), self.apply_healing, base_cases)
+    def damage_prompt(self):
+        base_cases = {
+            "b": lambda: self.true()
+        }
+        switcher = OptionSwitcher(len(self.active), self.apply_damage, base_cases)
 
-    clear()
-    self.print_healing_menu()
-    callback = None
-    while callback == None:
-      option = input("Select a number to heal combatant, or go (b)ack: ")
-      callback = switcher.get(option)
+        clear()
+        self.print_damage_menu()
+        callback = None
+        while callback is None:
+            option = input("Select a number to damage combatant, or go (b)ack: ")
+            callback = switcher.get(option)
 
-    return callback()
+        return callback()
 
-  def take_turn(self, index):
-    self.acted.append(self.ordered.pop(index))
-    return True
+    def heal_prompt(self):
+        base_cases = {
+            "b": lambda: self.true()
+        }
+        switcher = OptionSwitcher(len(self.encounter.combatants), self.apply_healing, base_cases)
 
-  def print_main_menu(self):
-    for combatant in self.acted:
-      print("\N{CHECK MARK} %s" % combatant.name)
-    print('')
-    for i in range(len(self.ordered)):
-      print("%d. %s (%d/%d)" % (
-        i + 1,
-        self.ordered[i].name,
-        self.ordered[i].hit_points[0],
-        self.ordered[i].hit_points[1]
-      ))
-    print('')
-    for combatant in self.encounter.combatants:
-      if combatant.hit_points[0] == 0 and combatant.hit_points[1] > 0:
-        print("\N{CROSS MARK} %s" % combatant.name)
+        clear()
+        self.print_healing_menu()
+        callback = None
+        while callback is None:
+            option = input("Select a number to heal combatant, or go (b)ack: ")
+            callback = switcher.get(option)
 
-  def apply_damage(self, index):
-    combatant = self.active[index]
-    damage = -1
-    while damage < 0:
-      try:
-        damage = int(input("How much damage to %s?: " % combatant.name))
-        if damage > -1:
-          break
-      except:
-        pass
-      print("Try again")
-    new_hp = combatant.hit_points[0] - damage
-    if combatant.hit_points[1] != 0:
-      new_hp = max(0, new_hp)
-    combatant.hit_points = (new_hp, combatant.hit_points[1])
-    if new_hp == 0:
-      self.active.pop(index)
-      self.ordered = [c for c in self.ordered if c.name != combatant.name]
-    return True
+        return callback()
 
-  def print_damage_menu(self):
-    for i in range(len(self.active)):
-      print("%d. %s (%d/%d)" % (
-        i + 1,
-        self.active[i].name,
-        self.active[i].hit_points[0],
-        self.active[i].hit_points[1]
-      ))
+    def take_turn(self, index):
+        self.acted.append(self.ordered.pop(index))
+        return True
 
-  def apply_healing(self, index):
-    combatant = self.encounter.combatants[index]
-    healing = -1
-    while healing < 0:
-      try:
-        healing = int(input("How many hit points to restore to %s?: " % combatant.name))
-        if healing > -1:
-          break
-      except:
-        pass
-      print("Try again")
-    new_hp = min(combatant.hit_points[0] + healing, combatant.hit_points[1])
-    combatant.hit_points = (new_hp, combatant.hit_points[1])
-    self.active = [c for c in self.encounter.combatants if c.hit_points[0] > 0 or c.hit_points[1] == 0]
-    return True
+    def print_main_menu(self):
+        for combatant in self.acted:
+            print("\N{CHECK MARK} %s" % combatant.name)
+        print('')
+        for i in range(len(self.ordered)):
+            print("%d. %s (%d/%d)" % (
+                i + 1,
+                self.ordered[i].name,
+                self.ordered[i].hit_points[0],
+                self.ordered[i].hit_points[1]
+            ))
+        print('')
+        for combatant in self.encounter.combatants:
+            if combatant.hit_points[0] == 0 and combatant.hit_points[1] > 0:
+                print("\N{CROSS MARK} %s" % combatant.name)
 
-  def print_healing_menu(self):
-    for i in range(len(self.encounter.combatants)):
-      print("%d. %s (%d/%d)" % (
-        i + 1,
-        self.encounter.combatants[i].name,
-        self.encounter.combatants[i].hit_points[0],
-        self.encounter.combatants[i].hit_points[1]
-      ))
+    def apply_damage(self, index):
+        combatant = self.active[index]
+        damage = -1
+        while damage < 0:
+            try:
+                damage = int(input("How much damage to %s?: " % combatant.name))
+                if damage > -1:
+                    break
+            except Exception:
+                pass
+            print("Try again")
+        new_hp = combatant.hit_points[0] - damage
+        if combatant.hit_points[1] != 0:
+            new_hp = max(0, new_hp)
+        combatant.hit_points = (new_hp, combatant.hit_points[1])
+        if new_hp == 0:
+            self.active.pop(index)
+            self.ordered = [c for c in self.ordered if c.name != combatant.name]
+        return True
+
+    def print_damage_menu(self):
+        for i in range(len(self.active)):
+            print("%d. %s (%d/%d)" % (
+                i + 1,
+                self.active[i].name,
+                self.active[i].hit_points[0],
+                self.active[i].hit_points[1]
+            ))
+
+    def apply_healing(self, index):
+        combatant = self.encounter.combatants[index]
+        healing = -1
+        while healing < 0:
+            try:
+                healing = int(input("How many hit points to restore to %s?: " % combatant.name))
+                if healing > -1:
+                    break
+            except Exception:
+                pass
+            print("Try again")
+        new_hp = min(combatant.hit_points[0] + healing, combatant.hit_points[1])
+        combatant.hit_points = (new_hp, combatant.hit_points[1])
+        self.active = [c for c in self.encounter.combatants if c.hit_points[0] > 0 or c.hit_points[1] == 0]
+        return True
+
+    def print_healing_menu(self):
+        for i in range(len(self.encounter.combatants)):
+            print("%d. %s (%d/%d)" % (
+                i + 1,
+                self.encounter.combatants[i].name,
+                self.encounter.combatants[i].hit_points[0],
+                self.encounter.combatants[i].hit_points[1]
+            ))
+
 
 class Combatant:
 
-  def __init__(self, name, initiative, hit_points):
-      self.name = name
-      self.initiative = initiative
-      self.hit_points = hit_points
+    def __init__(self, name, initiative, hit_points):
+        self.name = name
+        self.initiative = initiative
+        self.hit_points = hit_points
 
 
 class OptionSwitcher:
 
-  def __init__(self, num_options, action, base_cases={}):
-    self.action = action
-    self.cases = self.create_cases(num_options)
-    self.cases = {**self.cases, **base_cases}
+    def __init__(self, num_options, action, base_cases=None):
+        if base_cases is None:
+            base_cases = dict()
+        self.action = action
+        self.cases = self.create_cases(num_options)
+        self.cases = {**self.cases, **base_cases}
 
-  def create_cases(self, num_options):
-    cases = {}
-    for i in range(num_options):
-      cases[str(i+1)] = partial(self.action, i)
-    return cases
+    def create_cases(self, num_options):
+        cases = {}
+        for i in range(num_options):
+            cases[str(i + 1)] = partial(self.action, i)
+        return cases
 
-  def get(self, case):
-    return self.cases.get(case, None)
+    def get(self, case):
+        return self.cases.get(case, None)
 
 
 main()
